dcf40c83db265e67dc329a79e151045021c38f77
gtaws2-garkan <81730539+gtaws2-garkan@users.noreply.github.com>
2021-05-22 12:44:04 +0900

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,74 @@
+var object1 = {
+    name: "Portrait of a Young Man",
+    description: "1530s Bronzino (Agnolo di Cosimo di Mariano) Italian"
+};
+
+var object2 = {
+    name: "Soap Bubbles",
+    description: "ca. 1733–34 Jean Siméon Chardin French"
+};
+
+var object3 = {
+    name: "The Judgment of Paris",
+    description: "ca. 1528Lucas Cranach the Elder German"
+};
+
+var object4 = {
+    name: "Venus and Adonis",
+    description: "1550s Titian (Tiziano Vecellio) Italian"
+};
+
+var object5 = {
+    name: "Pandora",
+    description: "1914 Odilon Redon"
+};
+
+var object6 = {
+    name: "The Toilers of the Sea",
+    description: "1880–1885 Albert Pinkham Ryder"
+};
+
+var object7 = {
+    name: "Bullfight in a Divided Ring",
+    description: "Attributed to Goya (Francisco de Goya y Lucientes) Spanish"
+};
+
+var object8 = {
+    name: "Aristotle with a Bust of Homer",
+    description: "1653 Rembrandt (Rembrandt van Rijn) Dutch"
+};
+
+var object9 = {
+    name: "Woman with a Parrot",
+    description: "1866 Gustave Courbet French"
+};
+
+var object10 = {
+    name: "Madonna and Child",
+    description: "ca. 1290–1300 Duccio di Buoninsegna Italian"
+};
+
+var object11 = {
+    name: "Washington Crossing the Delaware",
+    description: "1851 Emanuel Leutze German American"
+};
+
+var object12 = {
+    name: "Tahitian Women Bathing",
+    description: "1892 Paul Gauguin French"
+};
+
+var object13 = {
+    name: "The Gardener",
+    description: "1882–83 Georges Seurat French"
+};
+
+var object14 = {
+    name: "Mäda Primavesi",
+    description: "1912–13 Gustav Klimt Austrian"
+};
+
+var object15 = {
+    name: "The Monet Family in Their Garden at Argenteuil",
+    description: "1874 Edouard Manet French"
+};
